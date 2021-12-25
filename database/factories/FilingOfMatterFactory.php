5b0da77ecd6d99b64115020c7eb88261279db965
Chairul Anam <chairuluniska@gmail.com>
2021-12-25 22:30:34 +0800

    V1

--- database/factories/FilingOfMatterFactory.php
+++ database/factories/FilingOfMatterFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\filingOfMatter;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class FilingOfMatterFactory extends Factory
@@ -13,8 +14,52 @@ class FilingOfMatterFactory extends Factory
      */
     public function definition()
     {
-        return [
-            //
+
+        $default = [
+            [
+                'name' => 'Perceraian',
+                'price' => '600000',
+                'description' => 'lorem ipsum'
+            ],
+            [
+                'name' => 'Ahli Waris',
+                'price' => '300000',
+                'description' => 'lorem ipsum'
+            ],
+            [
+                'name' => 'Wasiat',
+                'price' => '200000',
+                'description' => 'lorem ipsum'
+            ],
+            [
+                'name' => 'Ekonomi Syariah',
+                'price' => '100000',
+                'description' => 'lorem ipsum'
+            ],
+            [
+                'name' => 'Wakaf',
+                'price' => '250000',
+                'description' => 'lorem ipsum'
+            ],
+            [
+                'name' => 'Infaq dan Sedeqah',
+                'price' => '500000',
+                'description' => 'lorem ipsum'
+            ],
         ];
+
+
+        for ($i = 0; $i < count($default); $i++) {
+            $filingOfMatter = filingOfMatter::create(
+                [
+                    'icon' => 'folder.png',
+                    'name' => $default[$i]['name'],
+                    'price' => $default[$i]['price'],
+                    'description' => $default[$i]['description'],
+                ]
+            );
+        }
+
+        return $filingOfMatter;
     }
 }
